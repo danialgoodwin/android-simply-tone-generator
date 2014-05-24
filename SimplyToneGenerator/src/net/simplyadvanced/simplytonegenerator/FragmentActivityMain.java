31dd8dd8b0503c394a70151fa0948b5d0bfe0d41
Danial Goodwin <danialgoodwin@gmail.com>
2014-05-23 20:59:55 -0400

    Add fix for action bar

--- SimplyToneGenerator/src/net/simplyadvanced/simplytonegenerator/FragmentActivityMain.java
+++ SimplyToneGenerator/src/net/simplyadvanced/simplytonegenerator/FragmentActivityMain.java
@@ -19,6 +19,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.ShareActionProvider;
 import android.widget.TextView;
 
@@ -78,6 +79,16 @@ public class FragmentActivityMain extends Activity implements ActionBar.TabListe
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		// The Action Bar is a window feature. The feature must be requested
+	    // before setting a content view. Normally this is set automatically
+	    // by your Activity's theme in your manifest. The provided system
+	    // theme Theme.WithActionBar enables this for you. Use it as you would
+	    // use Theme.NoTitleBar. You can add an Action Bar to your own themes
+	    // by adding the element <item name="android:windowActionBar">true</item>
+	    // to your style definition.
+	    getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+	    
 		setContentView(R.layout.fragment_activity_main);
 
 	 	mHelperCommon = HelperCommon.getInstance(this);
--- SimplyToneGenerator/src/net/simplyadvanced/simplytonegenerator/HelperCommon.java
+++ SimplyToneGenerator/src/net/simplyadvanced/simplytonegenerator/HelperCommon.java
@@ -23,7 +23,7 @@ public class HelperCommon {
 	}
 	
 	
-	public static final String APP_VERSION = "1.3.1";
+	public static final String APP_VERSION = "1.3.2";
 
 	/** Determines whether or not all debug logs should show or not. If false, then hide all debug logcat messages. */
 	public static final boolean IS_DEBUG_MODE = false;
@@ -31,7 +31,7 @@ public class HelperCommon {
 	public static final String RECENT_UPDATES_PROMPT_TITLE = "Recent Updates - v" + APP_VERSION;
 	public static final String RECENT_UPDATES_PROMPT_MESSAGE =
 			"" +
-			"Version 1.3.1\n" +
+			"Version 1.3.2\n" +
 			"==========\n" +
 			"1. Bug fixes for a few devices.\n" +
 			"2. Slight performance increase.\n" +
